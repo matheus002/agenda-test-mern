e8f55fdb27fde50dff62b70363936e572b4eae82
Math002 <matheus002_@hotmail.com>
2020-03-11 20:33:52 -0300

    finalizando CRUD backend

--- routes/contacts.js
+++ routes/contacts.js
@@ -55,15 +55,61 @@ router.post('/', [auth, [
 // @route   PUT api/contacts/:id
 // @desc    Atualiar contato
 // @access  Private
-router.put('/:id', (req, res) => {
-    res.send('Atualizar contato');
+router.put('/:id', auth, async (req, res) => {
+    const { name, email, phone, type } = req.body;
+
+    //Criando obj contato
+    const contactFields = {};
+    if(name) contactFields.name = name;
+    if(email) contactFields.email = email;
+    if(phone) contactFields.phone = phone;
+    if(type) contactFields.type = type;
+
+    try {
+        let contact = await Contact.findById(req.params.id);
+
+        if(!contact) return res.status(404).json({ msg: 'Contato não encontrado'});
+
+        //Confirmanr que o contato pertence ao usuário
+        if(contact.user.toString() !== req.user.id) {
+            return res.status(401).json({ msg: 'Acesso não autorizado'});
+        }
+
+        contact = await Contact.findByIdAndUpdate(req.params.id, 
+            { $set: contactFields},
+            { new: true});
+
+            res.json(contact);
+
+    } catch (err) {  
+        console.error(err.message);
+        res.status(500).send('Server error');        
+    }
 });
 
 // @route   DELETE api/contacts/:id
-// @desc    Deletar contato
+// @desc    Deletar contatos
 // @access  Private
-router.delete('/:id', (req, res) => {
-    res.send('Deletar contato');
+router.delete('/:id', auth,  async (req, res) => {
+    try {
+        let contact = await Contact.findById(req.params.id);
+
+        if(!contact) return res.status(404).json({ msg: 'Contato não encontrado'});
+
+        //Confirmanr que o contato pertence ao usuário
+        if(contact.user.toString() !== req.user.id) {
+            return res.status(401).json({ msg: 'Acesso não autorizado'});
+        }
+
+        await Contact.findOneAndRemove(req.params.id);
+
+        res.json({ msg: 'Contato Removido'});
+
+    } catch (err) {  
+        console.error(err.message);
+        res.status(500).send('Server error');        
+    }
+    
 });
 
-module.exports = router;
+module.exports = router; 
