c02c104d4e3fff7f6f877d3531c9e726e0c47258
Math002 <matheus002_@hotmail.com>
2020-03-11 21:33:43 -0300

    Add client folder

---
+++ client/src/components/pages/About.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const About = () => {
+    return (
+        <div>
+            <h1>Sobre o app</h1>
+            <p className="my-1">
+                Aplicação full stack  com ReactJS para gerenciamento de contatos
+            </p>
+            <p className="bg-dark p">
+                <strong>Version: </strong> 1.0.0
+            </p>
+        </div>
+    )
+}
+
+export default About;
